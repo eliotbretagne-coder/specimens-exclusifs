201ead6095019b157ed232b5641afc1e36d3adfa
eliotbretagne-coder <eliotbretagne@gmail.com>
2026-07-08 16:23:14 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,13 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = "https://wykchkfnyktwxxgyvuei.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_pQ7a9xSQ9l_mGSsmRTEx9A_w82vjs-o";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storage: window.localStorage,
+  },
+});
 
 export const ADMIN_EMAIL = "eliotbretagne@gmail.com";
